f047adf426a296bc1ea925806fced67ced395f5d
bevuxyna <wannhorn@gmail.com>
2022-07-13 23:56:44 +0300

    commit

--- app.js
+++ app.js
@@ -1,16 +1,28 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const router = require('./routes/users');
+
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useNewUrlParser: true,
+});
 
 const { PORT = 3000 } = process.env;
 
 const app = express();
 
-mongoose.connect('mongodb://localhost:27017/mestodb', {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
+app.use(express.json());
+
+app.use((req, res, next) => {
+  req.user = {
+    _id: '62cf2655159a0d744a451c01',
+  };
+
+  next();
 });
 
+app.use('/users', router);
+
 app.listen(PORT, () => {
+  // eslint-disable-next-line no-console
   console.log(`App listening on port ${PORT}`);
 });
