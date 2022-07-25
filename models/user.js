fcb8aafc77fe70951b4b7856be67821bd45c1d2d
bevuxyna <wannhorn@gmail.com>
2022-07-25 21:39:52 +0300

    add email and password to user schema

--- models/user.js
+++ models/user.js
@@ -1,21 +1,36 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: [true, 'Введите имя'],
     minlength: [2, 'Текст должен быть не короче 2 символов'],
     maxlength: [30, 'Текст должен быть короче 30 символов'],
+    default: 'Жак-Ив Кусто',
   },
   about: {
     type: String,
-    required: [true, 'Добавьте информацию о себе'],
     minlength: [2, 'Текст должен быть не короче 2 символов'],
     maxlength: [30, 'Текст должен быть короче 30 символов'],
+    default: 'Исследователь',
   },
   avatar: {
     type: String,
-    required: true,
+    default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
+  },
+  email: {
+    type: String,
+    required: [true, 'Введите email'],
+    unique: true,
+    validate: {
+      validator: (email) => validator.isEmail(email),
+      message: 'Введен некорректный email',
+    },
+  },
+  password: {
+    type: String,
+    required: [true, 'Введите пароль'],
+    select: false,
   },
 });
 
