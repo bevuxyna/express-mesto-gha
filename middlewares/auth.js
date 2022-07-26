5d02eacdbbde3b3260780e16f37d9d1661dcf14a
bevuxyna <wannhorn@gmail.com>
2022-07-26 21:08:47 +0300

    add middleware for authorization

---
+++ middlewares/auth.js
@@ -0,0 +1,28 @@
+const jwt = require('jsonwebtoken');
+const { UNAUTHORIZED } = require('../utils/status');
+
+// eslint-disable-next-line consistent-return
+module.exports = (req, res, next) => {
+  const { authorization } = req.headers;
+
+  if (!authorization || !authorization.startsWith('Bearer ')) {
+    return res
+      .status(UNAUTHORIZED)
+      .send({ message: 'Необходима авторизация' });
+  }
+
+  const token = authorization.replace('Bearer ', '');
+  let payload;
+
+  try {
+    payload = jwt.verify(token, 'some-secret-key');
+  } catch (err) {
+    return res
+      .status(UNAUTHORIZED)
+      .send({ message: 'Необходима авторизация' });
+  }
+
+  req.user = payload; // записываем пейлоуд в объект запроса
+
+  next(); // пропускаем запрос дальше
+};
