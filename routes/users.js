38cd075c76654b33d7399be3c1d727cba2e7bbde
bevuxyna <wannhorn@gmail.com>
2022-07-27 22:32:36 +0300

    create user route

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const {
-  getUsers, getUser, updateUser, updateAvatar, getUserInfo,
+  getUsers, getUser, createUser, updateUser, updateAvatar, getUserInfo,
 } = require('../controllers/users');
 
 router.get('/', getUsers);
@@ -8,5 +8,6 @@ router.get('/:userId', getUser);
 router.get('/me', getUserInfo);
 router.patch('/me', updateUser);
 router.patch('/me/avatar', updateAvatar);
+router.post('/', createUser);
 
 module.exports = router;
