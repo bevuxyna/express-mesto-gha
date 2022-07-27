f82148737c7703adcdb90a1c03f545884b481302
bevuxyna <wannhorn@gmail.com>
2022-07-27 22:40:22 +0300

    commit

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const {
-  getUsers, getUser, createUser, updateUser, updateAvatar, getUserInfo,
+  getUsers, getUser, updateUser, updateAvatar, getUserInfo,
 } = require('../controllers/users');
 
 router.get('/', getUsers);
@@ -8,6 +8,5 @@ router.get('/:userId', getUser);
 router.get('/me', getUserInfo);
 router.patch('/me', updateUser);
 router.patch('/me/avatar', updateAvatar);
-router.post('/', createUser);
 
 module.exports = router;
