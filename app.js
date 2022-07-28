56d25ee88500e9d18e54cbfc7e64f15feea4dfd4
bevuxyna <wannhorn@gmail.com>
2022-07-28 22:42:07 +0300

    fix

--- app.js
+++ app.js
@@ -27,6 +27,12 @@ app.use((req, res) => {
   res.status(404).send({ message: 'Запрашиваемый ресурс не найден' });
 });
 
+app.use((err, req, res, next) => {
+  const { statusCode = 500, message } = err;
+  res.status(statusCode).send({ message: statusCode === 500 ? 'На сервере произошла ошибка' : message });
+  next();
+});
+
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`App listening on port ${PORT}`);
