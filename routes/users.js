88f69449a20c8a8cdf4109e43988d0165a8242b9
bevuxyna <wannhorn@gmail.com>
2022-07-29 00:27:27 +0300

    fix

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,15 @@
 const router = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
 const {
   getUsers, getUser, updateUser, updateAvatar, getUserInfo,
 } = require('../controllers/users');
 
 router.get('/', getUsers);
-router.get('/:userId', getUser);
+router.get('/:userId', celebrate({
+  params: Joi.object().keys({
+    id: Joi.string().required().hex().length(24),
+  }),
+}), getUser);
 router.get('/me', getUserInfo);
 router.patch('/me', updateUser);
 router.patch('/me/avatar', updateAvatar);
