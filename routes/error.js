89da123ff1cde2068f6447ef4c3cee76de10068f
bevuxyna <wannhorn@gmail.com>
2022-07-28 22:53:19 +0300

    fix

--- routes/error.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const NotFoundError = require('../errors/not-found-error');
-
-router.all('*', (req, res, next) => {
-  next(new NotFoundError('Запрашиваемый ресурс не найден'));
-});
-
-module.exports = router;
