07ac3d05b29ad12f5e4e24bc0a080819894f275a
bevuxyna <wannhorn@gmail.com>
2022-07-29 18:09:36 +0300

    add validation of request parameters and headers

--- middlewares/validator.js
+++ middlewares/validator.js
@@ -32,9 +32,7 @@ module.exports.validateCreateUser = celebrate({
     password: Joi.string().required().min(8),
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
-    avatar: Joi.string().regex(
-      /^(https?:\/\/)?([\w-]{1,32}\.[\w-]{1,32})[^\s@]*/,
-    ),
+    avatar: Joi.string().regex(/^(https?:\/\/)?([\w-]{1,32}\.[\w-]{1,32})[^\s@]*/),
   }),
 });
 
