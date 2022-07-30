305e69c185358432457d91316aa62083f177ede5
bevuxyna <wannhorn@gmail.com>
2022-07-30 12:03:07 +0300

    add required fields

--- middlewares/validator.js
+++ middlewares/validator.js
@@ -8,14 +8,14 @@ module.exports.validateUserId = celebrate({
 
 module.exports.validateUpdateUser = celebrate({
   body: Joi.object().keys({
-    name: Joi.string().min(2).max(30),
-    about: Joi.string().min(2).max(30),
+    name: Joi.string().required().min(2).max(30),
+    about: Joi.string().required().min(2).max(30),
   }),
 });
 
 module.exports.validateUpdateAvatar = celebrate({
   body: Joi.object().keys({
-    avatar: Joi.string().uri({ scheme: ['http', 'https'] }),
+    avatar: Joi.string().required().uri({ scheme: ['http', 'https'] }),
   }),
 });
 
